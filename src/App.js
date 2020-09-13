536804a0a1eb7e58eb5e8690e50f39dbcdbf7727
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2020-09-12 23:40:45 -0300

    Desafio Concluido

--- src/App.js
+++ src/App.js
@@ -1,31 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 
 import "./styles.css";
+import api from "./services/api";
 
 function App() {
-  async function handleAddRepository() {
-    // TODO
-  }
+    const [repositories, setRepositories] = useState([]);
 
-  async function handleRemoveRepository(id) {
-    // TODO
-  }
+    useEffect(() => {
+        api.get('repositories').then(response => {
+            setRepositories(response.data);
+        })
+    }, []);
 
-  return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    async function handleAddRepository() {
+        const response = await api.post('repositories', {
+            title: "Conceitos de ReactJs",
+            url: "https://github.com/rogerintel/conceitos-reactjs",
+            techs: ["ReactJs, Javascript, Yarn"]
+        });
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+        const repository = response.data;
 
-      <button onClick={handleAddRepository}>Adicionar</button>
-    </div>
-  );
+        setRepositories([...repositories, repository]);
+    }
+
+    async function handleRemoveRepository(id) {
+        const response = await api.delete('repositories/' + id);
+
+        if (response.status === 204) {
+            const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+            repositories.splice(repositoryIndex, 1);
+        }
+        setRepositories([...repositories]);
+    }
+
+    return (
+        <div>
+            <ul data-testid="repository-list">
+                {repositories.map(repository =>
+
+                    <li key={repository.id}>
+                        {repository.title}
+                        <button onClick={() => handleRemoveRepository(repository.id)}>
+                            Remover
+                        </button>
+                    </li>
+                )}
+            </ul>
+
+            <button onClick={handleAddRepository}>Adicionar</button>
+        </div>
+    );
 }
 
 export default App;
